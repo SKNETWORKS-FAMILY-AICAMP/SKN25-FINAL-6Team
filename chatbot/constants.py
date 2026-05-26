60a15904b2b965c37225303ba2d6fada2c52bdd7
Nayeon Kim <rosie1025@naver.com>
2026-05-26 10:02:22 +0900

    (#5) 우선 합치고, 안쓰는 코드 제거. 돌아가는지 확인 못함

--- chatbot/constants.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-MAX_SAFETY_RETRY = 2
-ROUTING_TARGET = ["rag_reply", "urgent_alert"]
-CATEGORY = ["결제", "인게임버그", "FAQ", "VOC"]
-FACTUALITY_THRESHOLD = 0.8
-HALLUCINATION_THRESHOLD = 0.3
-TOXICITY_THRESHOLD = 0.7
